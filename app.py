cfa276740633204a730796f3bd0b41b21516c147
samirathorizon <samir@horizon-tech.tn>
2024-01-11 22:03:42 -0800

    add branch prometheus_client

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
+view_metric = Counter('view', 'Product view', ['product'])
+buy_metric = Counter('buy', 'Product buy', ['product'])
 
 @app.route('/view/<id>')
 def view_product(id):
+    view_metric.labels(product=id).inc()
     return "View %s" % id
 
 @app.route('/buy/<id>')
 def buy_product(id):
+    buy_metric.labels(product=id).inc()
     return "Buy %s" % id
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
